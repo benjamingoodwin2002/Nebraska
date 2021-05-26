9d0eb52088ad061a72f80bcc130ac6056282dc28
benjamingoodwin2002 <84830100+benjamingoodwin2002@users.noreply.github.com>
2021-05-25 22:07:40 -0700

    Create NebraskaCharacter.cpp

---
+++ NebraskaCharacter.cpp
@@ -0,0 +1,442 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NebraskaCharacter.h"
+#include "Animation/AnimInstance.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/InputSettings.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "MotionControllerComponent.h"
+#include "XRMotionControllerBase.h" // for FXRMotionControllerBase::RightHandSourceId
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/SceneComponent.h"
+#include "Engine/PointLight.h"
+#include "DrawDebugHelpers.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "GameFramework/Actor.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Nebraska/InteractComponent.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
+
+//////////////////////////////////////////////////////////////////////////
+// ANebraskaCharacter
+
+ANebraskaCharacter::ANebraskaCharacter()
+{
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
+
+	// set our turn rates for input
+	BaseTurnRate = 45.f;
+	BaseLookUpRate = 45.f;
+	
+	// Create a CameraComponent	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->SetRelativeLocation(FVector(-39.56f, 1.75f, 64.f)); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	// Note: The ProjectileClass and the skeletal mesh/anim blueprints for Mesh1P, FP_Gun, and VR_Gun 
+	// are set in the derived blueprint asset named MyCharacter to avoid direct content references in C++.
+
+	// Create VR Controllers.
+	R_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("R_MotionController"));
+	R_MotionController->MotionSource = FXRMotionControllerBase::RightHandSourceId;
+	R_MotionController->SetupAttachment(RootComponent);
+	L_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("L_MotionController"));
+	L_MotionController->SetupAttachment(RootComponent);
+
+	SprintSpeedMultiply = 2.5f;
+	CanSprint = true;
+	StrafeSpeed = 2.2f;
+	Landed = true;
+	DoOnce = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(FirstPersonCameraComponent);
+
+	glowstick = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("glowstick"));
+	glowstick->SetupAttachment(SpringArm);
+
+	HoldingComponent = CreateDefaultSubobject<USceneComponent>(TEXT("HoldingComponent"));
+	HoldingComponent->SetupAttachment(SpringArm);
+
+	CurrentItem = NULL;
+	bCanMove = true;
+	bInspecting = false;
+
+	GrabberClass = CreateDefaultSubobject<UInteractComponent>(TEXT("GrabberClass"));
+
+	PhysicsHandle = CreateDefaultSubobject<UPhysicsHandleComponent>(TEXT("PhysicsHandle"));
+
+}
+
+void ANebraskaCharacter::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+
+	PitchMax = GetWorld()->GetFirstPlayerController()->PlayerCameraManager->ViewPitchMax;
+	PitchMin = GetWorld()->GetFirstPlayerController()->PlayerCameraManager->ViewPitchMin;
+	
+}
+
+void ANebraskaCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (GrabberClass != nullptr)
+	{
+		GrabberClass->TraceHandleLocation(PhysicsHandle, FirstPersonCameraComponent);
+	}
+
+	//Interact Object
+
+	//PickUp Object
+
+	Start = FirstPersonCameraComponent->GetComponentLocation();
+	ForwardVector = FirstPersonCameraComponent->GetForwardVector();
+	End = ((ForwardVector * 330.0f) + Start);
+
+	//DrawDebugLine(GetWorld(), Start, End, FColor::Green, false, 1, 0, 1);
+
+	if (!bHoldingItem)
+	{
+		if (GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, DefaultComponentQueryParams, DefaultResponseParam))
+		{
+			if (Hit.GetActor()->GetClass()->IsChildOf(AGrabObject::StaticClass()))
+			{
+				CurrentItem = Cast<AGrabObject>(Hit.GetActor());
+			}
+		}
+		else
+		{
+			CurrentItem = NULL;
+		}
+	}
+
+	if (bInspecting)
+	{
+		if (bHoldingItem)
+		{
+			GetWorld()->GetFirstPlayerController()->PlayerCameraManager->ViewPitchMax = 179.9000002f;
+			GetWorld()->GetFirstPlayerController()->PlayerCameraManager->ViewPitchMin = -179.9000002f;
+			CurrentItem->RotateObject();
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Input
+
+void ANebraskaCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	// set up gameplay key bindings
+	check(PlayerInputComponent);
+
+	// Bind jump events
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &ANebraskaCharacter::sprint);
+	PlayerInputComponent->BindAction("Sprint", IE_Released, this, &ANebraskaCharacter::stopsprinting);
+
+	PlayerInputComponent->BindAction("GlowStick", IE_Pressed, this, &ANebraskaCharacter::GlowStick1);
+
+	PlayerInputComponent->BindAction("Interact", IE_Pressed, this, &ANebraskaCharacter::Grab);
+	//PlayerInputComponent->BindAction("Interact", IE_Released, this, &ANebraskaCharacter::GrabR);
+
+	PlayerInputComponent->BindAction("Throwing", IE_Pressed, this, &ANebraskaCharacter::Throw);
+	PlayerInputComponent->BindAction("Throwing", IE_Released, this, &ANebraskaCharacter::ThrowR);
+
+	PlayerInputComponent->BindAction("Interact", IE_Pressed, this, &ANebraskaCharacter::Interact);
+
+	PlayerInputComponent->BindAction("Inspect", IE_Pressed, this, &ANebraskaCharacter::Inspect);
+	PlayerInputComponent->BindAction("Inspect", IE_Released, this, &ANebraskaCharacter::InspectR);
+
+	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &ANebraskaCharacter::OnResetVR);
+
+	// Bind movement events
+	PlayerInputComponent->BindAxis("MoveForward", this, &ANebraskaCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ANebraskaCharacter::MoveRight);
+
+	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
+	// "turn" handles devices that provide an absolute delta, such as a mouse.
+	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("TurnRate", this, &ANebraskaCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &ANebraskaCharacter::LookUpAtRate);
+}
+
+void ANebraskaCharacter::OnResetVR()
+{
+	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+}
+
+//Commenting this section out to be consistent with FPS BP template.
+//This allows the user to turn without using the right virtual joystick
+
+//void ANebraskaCharacter::TouchUpdate(const ETouchIndex::Type FingerIndex, const FVector Location)
+//{
+//	if ((TouchItem.bIsPressed == true) && (TouchItem.FingerIndex == FingerIndex))
+//	{
+//		if (TouchItem.bIsPressed)
+//		{
+//			if (GetWorld() != nullptr)
+//			{
+//				UGameViewportClient* ViewportClient = GetWorld()->GetGameViewport();
+//				if (ViewportClient != nullptr)
+//				{
+//					FVector MoveDelta = Location - TouchItem.Location;
+//					FVector2D ScreenSize;
+//					ViewportClient->GetViewportSize(ScreenSize);
+//					FVector2D ScaledDelta = FVector2D(MoveDelta.X, MoveDelta.Y) / ScreenSize;
+//					if (FMath::Abs(ScaledDelta.X) >= 4.0 / ScreenSize.X)
+//					{
+//						TouchItem.bMoved = true;
+//						float Value = ScaledDelta.X * BaseTurnRate;
+//						AddControllerYawInput(Value);
+//					}
+//					if (FMath::Abs(ScaledDelta.Y) >= 4.0 / ScreenSize.Y)
+//					{
+//						TouchItem.bMoved = true;
+//						float Value = ScaledDelta.Y * BaseTurnRate;
+//						AddControllerPitchInput(Value);
+//					}
+//					TouchItem.Location = Location;
+//				}
+//				TouchItem.Location = Location;
+//			}
+//		}
+//	}
+//}
+
+void ANebraskaCharacter::MoveForward(float Value)
+{
+	if (Value != 0.0f && bCanMove)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorForwardVector(), Value);
+		if (Value < 0.0f || Value > 0.0f)
+		{
+			if (Landed == true)
+			{
+				SetTimers();
+			}
+		}
+		else if (Value == 0.0f)
+		{
+			ClearTimers();
+		}
+
+		if (GetCharacterMovement()->MaxWalkSpeed >= 301.0f)
+		{
+			GetWorld()->GetFirstPlayerController()->PlayerCameraManager->StartCameraShake(URunHeadbob, 1);
+		}
+		else
+		{
+			GetWorld()->GetFirstPlayerController()->PlayerCameraManager->StartCameraShake(UHeadbob, 1);
+		}
+	}
+}
+
+void ANebraskaCharacter::MoveRight(float Value)
+{
+	if (Value != 0.0f && bCanMove)
+	{
+		if (Value < 0.0f || Value > 0.0f)
+		{
+			if (Landed == true)
+			{
+				SetTimers();
+			}
+		}
+		else if (Value == 0.0f)
+		{
+			ClearTimers();
+		}
+		// add movement in that direction
+		AddMovementInput(GetActorRightVector(), Value);
+		GetWorld()->GetFirstPlayerController()->PlayerCameraManager->StartCameraShake(UHeadbob, 1);
+	}
+}
+
+void ANebraskaCharacter::TurnAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ANebraskaCharacter::LookUpAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ANebraskaCharacter::sprint()
+{
+	if (CanSprint == true && GetCharacterMovement()->MaxWalkSpeed > 299.9)
+	{
+		if (DoOnce == true)
+		{
+			GetCharacterMovement()->MaxWalkSpeed *= SprintSpeedMultiply;
+			DoOnce = false;
+		}
+	}
+}
+
+void ANebraskaCharacter::stopsprinting()
+{
+	if (CanSprint == true)
+	{
+		if (DoOnce == false)
+		{
+			GetCharacterMovement()->MaxWalkSpeed /= SprintSpeedMultiply;
+			DoOnce = true;
+		}
+	}
+}
+
+void ANebraskaCharacter::Interact()
+{
+	if (CurrentItem && !bInspecting)
+	{
+		ToggleItemPickUp();
+	}
+}
+
+void ANebraskaCharacter::Inspect()
+{
+	if (bHoldingItem)
+	{
+		LastRotation = GetControlRotation();
+		ToggleMovement();
+	}
+	else
+	{
+		bInspecting = true;
+	}
+}
+
+void ANebraskaCharacter::InspectR()
+{
+	if (bInspecting && bHoldingItem)
+	{
+		GetController()->SetControlRotation(LastRotation);
+		GetWorld()->GetFirstPlayerController()->PlayerCameraManager->ViewPitchMax = PitchMax;
+		GetWorld()->GetFirstPlayerController()->PlayerCameraManager->ViewPitchMin = PitchMin;
+		ToggleMovement();
+	}
+	else
+	{
+		bInspecting = false;
+	}
+}
+
+void ANebraskaCharacter::ToggleMovement()
+{
+	bCanMove = !bCanMove;
+	bInspecting = !bInspecting;
+	FirstPersonCameraComponent->bUsePawnControlRotation = !FirstPersonCameraComponent->bUsePawnControlRotation;
+	bUseControllerRotationYaw = !bUseControllerRotationYaw;
+}
+
+void ANebraskaCharacter::ToggleItemPickUp()
+{
+	if (CurrentItem)
+	{
+		bHoldingItem = !bHoldingItem;
+		CurrentItem->PickUpObject();
+
+		if (!bHoldingItem)
+		{
+			CurrentItem = NULL;
+		}
+	}
+}
+
+void ANebraskaCharacter::GlowStick1()
+{
+	glowstick->ToggleVisibility(true);
+	glowlighton();
+}
+
+void ANebraskaCharacter::Grab()
+{
+	if (GrabberClass != nullptr)
+	{
+		switch (GrabberClass->GetIsObjectHeld())
+		{
+			case true:
+			{
+				if (PhysicsHandle != nullptr)
+				{
+					if (GrabberClass->Release(PhysicsHandle))
+					{
+						UE_LOG(LogTemp, Warning, TEXT("Released"));
+					}
+					else
+					{
+						UE_LOG(LogTemp, Warning, TEXT("Faliure to release"));
+					}
+				}
+				break;
+			}
+			case false:
+			{
+				if (PhysicsHandle != nullptr && FirstPersonCameraComponent != nullptr)
+				{
+					if (GrabberClass->Grab(this, PhysicsHandle, FirstPersonCameraComponent))
+					{
+						UE_LOG(LogTemp, Warning, TEXT("Grabbed"));
+					}
+					else
+					{
+						UE_LOG(LogTemp, Warning, TEXT("Faliure to grab"));
+					}
+				}
+				break;
+			}
+		}
+	}
+}
+
+void ANebraskaCharacter::GrabR()
+{
+
+}
+
+void ANebraskaCharacter::Throw()
+{
+	bMouseDown = true;
+	bMouseUp = false;
+	if (GrabberClass != nullptr)
+	{
+		if (PhysicsHandle != nullptr && FirstPersonCameraComponent != nullptr)
+		{
+			if (GrabberClass->GetIsObjectHeld())
+			{
+				if (GrabberClass->Throw(PhysicsHandle, FirstPersonCameraComponent, bMouseDown))
+				{
+					UE_LOG(LogTemp, Warning, TEXT("Throw"));
+				}
+				else
+				{
+					UE_LOG(LogTemp, Warning, TEXT("failure to throw"));
+				}
+			}
+		}
+	}
+}
+
+void ANebraskaCharacter::ThrowR()
+{
+	bMouseUp = true;
+	bMouseDown = false;
+}
