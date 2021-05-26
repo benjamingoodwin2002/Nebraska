0a20158d454f97da807f06beafd826af35a5bc11
benjamingoodwin2002 <84830100+benjamingoodwin2002@users.noreply.github.com>
2021-05-25 22:10:10 -0700

    Create Headbob.h

---
+++ Headbob.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraShakeBase.h"
+#include "Camera/CameraShake.h"
+#include "Headbob.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEBRASKA_API UHeadbob : public UMatineeCameraShake
+{
+	GENERATED_BODY()
+
+public:
+	UHeadbob();
+
+};
