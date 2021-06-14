81759e72abcec444ce3c01b5a25690862ed6b627
benjamingoodwin2002 <84830100+benjamingoodwin2002@users.noreply.github.com>
2021-06-14 15:58:09 -0700

    Create ExaminationComponent.h

---
+++ ExaminationComponent.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "NebraskaCharacter.h"
+#include "Blueprint/UserWidget.h"
+#include "ExaminationComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class NEBRASKA_API UExaminationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UExaminationComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+
+	//UPROPERTY(EditAnywhere)
+	//ANebraskaCharacter* FirstPersonPlayer;
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION()
+	void Examin2();
+
+	UFUNCTION()
+	void Examin2End();
+
+	UPROPERTY(EditAnywhere)
+	bool IsExamin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UUserWidget> WidgetClass;
+
+	UPROPERTY(VisibleInstanceOnly)
+	class UUserWidget* ExamWidget;
+
+};
