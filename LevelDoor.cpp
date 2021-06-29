47c4997a1dd52af58ebef094987d89c238bc1be0
benjamingoodwin2002 <84830100+benjamingoodwin2002@users.noreply.github.com>
2021-06-28 19:21:53 -0700

    Create LevelDoor.cpp

---
+++ LevelDoor.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LevelDoor.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ALevelDoor::ALevelDoor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
+	RootComponent = DoorMesh;
+
+}
+
+// Called when the game starts or when spawned
+void ALevelDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALevelDoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ALevelDoor::OpenDoor(ANebraskaCharacter* Character, UNebraskaGameInstance* Instance)
+{
+	//UGameplayStatics::OpenLevel(DoorMesh, LevelName);
+}
+
