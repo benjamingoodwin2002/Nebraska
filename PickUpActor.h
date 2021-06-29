699ee9249843283b7f0ba3b693ed025c621ca509
benjamingoodwin2002 <84830100+benjamingoodwin2002@users.noreply.github.com>
2021-06-28 19:24:47 -0700

    Create PickUpActor.h

---
+++ PickUpActor.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Nebraska/NebraskaCharacter.h"
+#include "PickUpActor.generated.h"
+
+UCLASS()
+class NEBRASKA_API APickUpActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickUpActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnPickedUp();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* Image;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MyMesh;
+
+	UPROPERTY(EditAnywhere)
+	bool PickedUp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText UseActionText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText ItemDescription;
+
+	UPROPERTY(EditAnywhere)
+	class USoundBase* PickUpSound;
+
+	virtual void Use(class ANebraskaCharacter* Character);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnUse(class ANebraskaCharacter* Character);
+};
