619016d2eacdeefd2b60bf6e4b60ab39cf11ed2f
benjamingoodwin2002 <84830100+benjamingoodwin2002@users.noreply.github.com>
2021-06-28 19:23:06 -0700

    Create NebraskaGameInstance.cpp

---
+++ NebraskaGameInstance.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NebraskaGameInstance.h"
+
+#include "Blueprint/UserWidget.h"
+#include "UObject/ConstructorHelpers.H"
+
+UNebraskaGameInstance::UNebraskaGameInstance()
+{
+	C1Locked = true;
+	HealthInst = 1.0f;
+}
+
+//void UNebraskaGameInstance::Init()
+//{
+//	UE_LOG(LogTemp, Warning, Text("We are here"), *testwidgetClass->GetName());
+//}
+
+//void UNebraskaGameInstance::showwidget()
+//{
+	//Create WIdget and add it to viewpoint
+//	UUserWidget* test = CreateWidget<UUserWidget>(this, testwidgetClass);
+//	test->AddToViewport();
+
+//	APlayerController* PlayerController = GetFirstLocalPlayerController();
+
+//}
